cc871366e1c50a26914a3b46d464fae78e025806
Manish Kumar <43175313+mkubiophysics@users.noreply.github.com>
2022-09-07 15:25:48 +0530

    Update run_nucletide.pl

--- run_nucletide.pl
+++ run_nucletide.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl;
+# $ARGV[0]= your query file (gene sequences in fasta format)
     system "nhmmscan -E 1e-6 --tblout nhmmscan_output nARGhmm/Total_95_families_nucleotide.hmm $ARGV[0]";
 
 	
