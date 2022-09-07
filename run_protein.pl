4c3caf171c5a1b1e355bf0e685d74321beb65a10
Manish Kumar <43175313+mkubiophysics@users.noreply.github.com>
2022-09-07 15:26:32 +0530

    Update run_protein.pl

--- run_protein.pl
+++ run_protein.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl;
 
-# $ARGV[0]= Your Query file Protein in fasta
+# $ARGV[0]= Your Query file (Protein sequences in fasta format)
     system "hmmscan -E 1e-6 --tblout phmmscan_output pARGhmm/TOTAL_95_families.hmm $ARGV[0]";
 
 	
